4cf086fed3ae628dae522acc02ea030a6497a441
Rishit-dagli <rishit.dagli@gmail.com>
2022-10-11 17:48:39 +0000

    Deploying to gh-pages from @ Rishit-dagli/cs-theory-notes@54c384b7e21b9345a479d0761a8adb6893a4c107 🚀

--- images.pl
+++ images.pl
@@ -2,6 +2,10 @@
 # Associate images original text with physical files.
 
 
+$key = q/(q,delta,epsilon);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img17.svg"
+ ALT="$(q, \delta, \epsilon)$">|; 
+
 $key = q/2;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 1.72ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img6.svg"
  ALT="$2$">|; 
@@ -14,9 +18,37 @@ $key = q/C;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 1.81ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
  ALT="$C$">|; 
 
+$key = q/Dec^x(i)=b_i;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img19.svg"
+ ALT="$Dec^x(i) = b_i$">|; 
+
 $key = q/b_i;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.19ex; vertical-align: -0.46ex; " SRC="|."$dir".q|img2.svg"
  ALT="$b_i$">|; 
+
+$key = q/bigtriangleup(x,Enc(b))leqdeltan;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img18.svg"
+ ALT="$\bigtriangleup(x, Enc(b)) \leq \delta n$">|; 
+
+$key = q/bin{0,1}^krightarrowxin{0,1}^n;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.73ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img13.svg"
+ ALT="$b \in {0,1}^k \rightarrow x \in {0,1}^n$">|; 
+
+$key = q/geqfrac{1}{2}+epsilon;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.98ex; vertical-align: -0.92ex; " SRC="|."$dir".q|img20.svg"
+ ALT="$\geq \frac{1}{2} + \epsilon$">|; 
+
+$key = q/hat{b_i}in{0,1};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.90ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img16.svg"
+ ALT="$\hat{b_i} \in {0,1}$">|; 
+
+$key = q/i;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.76ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img21.svg"
+ ALT="$i$">|; 
+
+$key = q/iin[k];MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img15.svg"
+ ALT="$i \in [k]$">|; 
 
 $key = q/k;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 1.84ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img3.svg"
@@ -49,6 +81,10 @@ $cached_env_img{$key} = q|<IMG STYLE="height: 1.68ex; vertical-align: -0.57ex; "
 $key = q/q=3;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.17ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img8.svg"
  ALT="$q = 3$">|; 
+
+$key = q/x;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.23ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img14.svg"
+ ALT="$x$">|; 
 
 1;
 
